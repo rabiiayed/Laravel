b46d8b09e15f9de7bfbbc0d86602e80fc54ee0fd
Rabii <ayedrabii@gmail.com>
2018-12-29 11:06:20 +0100

    Create post section is  working

--- app/User.php
+++ app/User.php
@@ -45,6 +45,9 @@ class User extends Authenticatable
 
     }
 
+public function posts(){
+        return $this->hasMany('App\Post');
 
+}
 
 }
---
+++ resources/views/admin/posts/edit.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+
+
+
+
+@section('content')
+
+
+    <h1>Edit post</h1>
+
+
+
+@stop
